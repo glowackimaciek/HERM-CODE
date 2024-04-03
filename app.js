a5c5976e3958b964adbeff556b40748601ea6cde
Maciej <macglowacki@gmail.com>
2024-04-03 14:36:08 +0200

    Dodanie routów

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const aboutRouter = require('./routes/about');
+const contactRouter = require('./routes/contact');
+const offerRouter = require('./routes/offer');
+const loginRouter = require('./routes/login');
+const registerRouter = require('./routes/register');
 
 const app = express();
 
@@ -20,7 +24,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/o-mnie', aboutRouter);
+app.use('/kontakt', contactRouter);
+app.use('/oferta', offerRouter);
+app.use('/logowanie', loginRouter);
+app.use('/rejestracja', registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
